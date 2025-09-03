dd6c1d2eb180b91e9b48e34a46f3e4b96f5676a1
leonardogaravito <leonardo123garavito@gmail.com>
2025-09-02 20:15:28 -0500

    Proyecto JS

---
+++ 01-HolaMundo/Holamundo.js
@@ -0,0 +1,32 @@
+console.log('Hola mundo');
+
+let cajaDeAndy = "woody";
+
+console.log(cajaDeAndy);
+
+let edad = 25;
+let nombre = "leonardo"
+
+console.log('mi nombre es '+ nombre+" y tengo "+ edad + "años");
+
+//array
+
+let frutas =['banano', 'fresa', 'mango'];
+
+console.log (frutas);
+
+
+console.log (frutas[1]);
+console.log (frutas[0]);
+
+let frase ="JavaScript es extremandamente genial"
+
+console.log(frase);
+console.log (frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+// operaciones matematica
+const suma = 3 + 12;
+
+console.log(suma)
